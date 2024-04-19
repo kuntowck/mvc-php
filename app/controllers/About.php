1c3bb09dda75b8b4d60b3437eb25dd68dbf5575d
kuntowck <kunto312002@gmail.com>
2024-04-19 20:41:47 +0700

    controller: method dan params

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About
+{
+   public function index($nama, $role)
+   {
+      echo "Halo, nama saya $nama, saya adalah seorang $role";
+   }
+
+   public function page()
+   {
+      echo 'About/page';
+   }
+}
