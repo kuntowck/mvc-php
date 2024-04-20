685645aeaab14ef178b75375aa5536599bd1e05c
kuntowck <kunto312002@gmail.com>
2024-04-20 13:03:20 +0700

    view: home, about, page, judul

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,25 @@
 <?php
 
-class About
+class About extends Controller
 {
-   public function index($nama, $role)
+   public function index($nama = 'user', $role = 'developer', $umur = 22)
    {
-      echo "Halo, nama saya $nama, saya adalah seorang $role";
-   }
+      $data['nama'] = $nama;
+      $data['role'] = $role;
+      $data['umur'] = $umur;
+      $data['judul'] = 'About me';
 
+      $this->view('templates/header', $data);
+      $this->view('about/index', $data);
+      $this->view('templates/footer');
+   }
+   
    public function page()
    {
-      echo 'About/page';
+      $data['judul'] = 'My page';
+      
+      $this->view('templates/header', $data);
+      $this->view('about/page');
+      $this->view('templates/footer');
    }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+   <h1>About me :D</h1>
+   <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, dan saya adalah <?= $data['role']; ?>.</p>
